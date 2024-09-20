4fdeff520d1f470e3fc51631ffeef8fd4602be59
RemiL00 <remi.loosveldt@gmail.com>
2024-09-20 09:33:01 +0200

    Première version du cours de JS

---
+++ Cours.js
@@ -0,0 +1,11 @@
+//console.log("Hello world !")
+//déclaration de variable
+let firstName = "Bob"
+let age = 34
+
+//changer sa valeur
+firstName = "bob"
+age = 45
+
+//afficher dans la console
+console.log(firstName.length)
