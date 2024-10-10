84e90593cda750e293531fcbf6df46248c2665a6
RemiL00 <remi.loosveldt@gmail.com>
2024-10-10 09:01:31 +0200

    Exercice 2 du TD

--- Cours.js
+++ Cours.js
@@ -1,11 +1,56 @@
 //console.log("Hello world !")
 //déclaration de variable
-let firstName = "Bob"
-let age = 34
+//let firstName = "Bob"
+//let age = 34
 
 //changer sa valeur
-firstName = "bob"
-age = 45
+//firstName = "bob"
+//age = 45
 
 //afficher dans la console
-console.log(firstName.length)
+//console.log(firstName.length)
+
+//fonctions
+//function sayHello(firstName){
+//    console.log("hello "+firstName);
+//}
+
+//function schoolName(){
+//    return "ENSEA"
+//}
+
+//sayHello("Bob")
+//sayHello("Leila")
+
+//console.log(schoolName());
+
+
+//aray
+// users = ["bob", "ron", "yassin"]
+
+// for (let index = 0; index < users.length; index++) {
+//     const element = users[index];
+//     console.log(element);
+
+// }
+
+// users.forEach(element => {
+//     console.log(element)
+// });
+
+users = [
+    {
+        firstName = "bob"
+        age = 23
+    }
+    {
+        firstName = "ron"
+        age = 45
+
+    }
+    {
+        firstName = "yassin"
+        age = 22
+
+    }
+]    
---
+++ TD_exercice2.js
@@ -0,0 +1,9 @@
+let student = {
+    Name : "Marie",
+    age : 20,
+    courses : []
+
+}
+console.log(student.age);
+student.age = 21;
+console.log(student.age);
