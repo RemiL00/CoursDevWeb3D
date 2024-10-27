a3198623591131e1506773b2f297c37301310486
RemiL00 <remi.loosveldt@gmail.com>
2024-10-27 19:57:21 +0100

    Partie PUT terminé

--- Rest_API/index.js
+++ Rest_API/index.js
@@ -19,27 +19,35 @@ app.post("/", (req, res) => {
 	const newId = lastId + 1
 
 	// créer le nouvel utilisateur avec les données du corps de la requête et l'ID calculé
-	const newUser = {
-		firstName,
-		lastName,
-		id: newId,
-	}
+	const newUser = { firstName, lastName, id: newId,}
 
 	// ajouter le nouvel utilisateur à notre liste d'utilisateurs en utilisant la méthode 'push'
 	users.push(newUser)
+
 	// envoyer le code de statut 201 (créé) et les données du nouvel utilisateur afin de confirmer au client.
 	res.status(201).json(newUser)
 });
 
 //PUT : Modifier un utilisateur basé sur les données envoyées dans le corps(body) et le paramètre passé dans l'URL
-app.put("/", (req, res) => {
+app.put("/:id", (req, res) => {
 	// récupérer toutes les données qui arrivent dans le corps de la requête (body)
-	const {
-		firstName, lastName 
-	} = req.body
+	const { firstName, lastName } = req.body
 	const id = parseInt(req.params.id)
+
 	// trouve son index, verifier si le userIndex est positive
 	const userIndex = users.findIndex((user) => user.id === id)
+
+	// utilisateur non trouvé
+	if (userIndex < 0)
+		return res.status(404).json({ msg: "utilisateur non trouvé" })
+
+	// si il est trouvé, nous vérifions quelles valeurs ont été envoyées
+	if (firstName) users[userIndex].firstName = firstName
+	if (lastName) users[userIndex].lastName = lastName
+	res.json({
+		msg: "utilisateur mis à jour",
+		user: users[userIndex],
+	})
 });
 
 //DELETE : Modifier un utilisateur basé sur les données envoyées dans le corps(body) et le paramètre passé dans l'URL
