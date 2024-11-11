8ac683b799d06ca56076a3a8bc57ca99602ddede
RemiL00 <remi.loosveldt@gmail.com>
2024-11-11 18:18:02 +0100

    Debut de réecriture des requetes(GET,PUT...) pour passer de js à SQL

--- Rest_API/routes/users.js
+++ Rest_API/routes/users.js
@@ -1,26 +1,18 @@
 const express = require("express")
 const router = express.Router()
 
-const usersArray = [
-	{ id: 1, firstName: 'John', lastName: 'Doe', role: 'admin' },
-	{ id: 2, firstName: 'Jane', lastName: 'Smith', role: 'user' },
-	{ id: 3, firstName: 'Alice', lastName: 'Johnson', role: 'moderator' },
-	{ id: 4, firstName: 'Bob', lastName: 'Brown', role: 'user' },
-	{ id: 5, firstName: 'Charlie', lastName: 'Davis', role: 'admin' }
-];
+const db = require("../database")
 
+// GET :ALL
 router.get("/users", (req, res) => {
-    res.json(usersArray)
+    db.all('SELECT * FROM users', [], (err, rows) => {
+		if (err) {
+		  res.status(500).json({ error: err.message });
+		} else {
+		  res.json(rows);
+		}
+	  });	
 })
-
-module.exports = router
-
-// GET 
-router.get('/users', (req, res) => {
-    res.json({
-		msg:"Welcome to my users API !"
-	})
-});
 
 // GET : Un seul utilisateur
 router.get('/users/:id', (req, res) => {
@@ -96,3 +88,5 @@ router.delete("/users/:id", (req, res) => {
 		msg: "utilisateur suprimée",
 	})
 });
+
+module.exports = router
